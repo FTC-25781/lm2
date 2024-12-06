217672073992499bb2ac08d14a6d716a4f47ad47
yuvan-ramesh <helloyuvan@gmail.com>
2024-12-05 20:18:57 -0800

    Auto Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRedActiveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRedActiveAuto.java
@@ -5,6 +5,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -35,95 +36,234 @@ public class BlueRedActiveAuto extends LinearOpMode {
 
         Action bluered1 = drive.actionBuilder(new Pose2d(62.8,20.5,0))
                 .strafeToLinearHeading(new Vector2d(72.0, 54.7), -45) //first time going under basket
-                .stopAndAdd(new Action() {
-                    @Override
-                    public boolean run(@NonNull TelemetryPacket telemetryPacket) { //dep slides to drop pos
-                        robot.depositSlide.extendDepositMainSlide();
-                        robot.depositV4B.setWristDropPosition();
-                        robot.depositClaw.openDepositClaw();
-                        robot.intakeV4B.setWristDefaultPosition();
-                        robot.depositV4B.setWristPickPosition();
-                        robot.depositSlide.retractDepositMainSlide();
-                        return true;
-                }})
+                .stopAndAdd(new SequentialAction(
+                        (p) -> {
+                            robot.depositSlide.extendDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4),
+                        (p) -> {
+                            robot.depositV4B.setWristDropPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositClaw.openDepositClaw();
+                            robot.intakeV4B.setWristDefaultPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositV4B.setWristPickPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositSlide.retractDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4)
+
+                ))
                 .strafeToLinearHeading(new Vector2d(85.5,47.95), 0) //first sample
-                .stopAndAdd(new Action() { //intake slides go out
-                    @Override
-                    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-                        robot.intakeSlide.extendMainSlide();
-                        robot.intakeClaw.openClaw();
-                        robot.intakeClaw.setOrientationIncrease();
-                        robot.intakeV4B.setWristPickPosition();
-                        robot.intakeClaw.closeClaw();
-                        robot.intakeV4B.setWristDropPosition();
-                        robot.depositClaw.openDepositClaw();
-                        robot.intakeClaw.setOrientationIncrease();
-                        return true;
-                }})
+                .stopAndAdd(new SequentialAction(
+                        (p) -> {
+                            robot.intakeSlide.extendMainSlide();
+                            robot.intakeClaw.openClaw();
+                            return false;
+                        },
+                        new SleepAction(2),
+                        (p) -> {
+                            robot.intakeClaw.setOrientationIncrease();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeV4B.setWristPickPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeClaw.closeClaw();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeV4B.setWristDropPosition();
+                            robot.depositClaw.openDepositClaw();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeClaw.setOrientationIncrease();
+                            return false;
+                        },
+                        new SleepAction(1)
+
+                ))
                 .strafeToLinearHeading(new Vector2d(72.0, 54.7), -45) //going back under basket
-                .stopAndAdd(new Action() {
-                    @Override
-                    public boolean run(@NonNull TelemetryPacket telemetryPacket) { //dep slides to drop pos
-                        robot.depositSlide.extendDepositMainSlide();
-                        robot.depositV4B.setWristDropPosition();
-                        robot.depositClaw.openDepositClaw();
-                        robot.intakeV4B.setWristDefaultPosition();
-                        robot.depositV4B.setWristPickPosition();
-                        robot.depositSlide.retractDepositMainSlide();
-                        return true;
-                    }})
+                .stopAndAdd(new SequentialAction(
+                        (p) -> {
+                            robot.depositSlide.extendDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4),
+                        (p) -> {
+                            robot.depositV4B.setWristDropPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositClaw.openDepositClaw();
+                            robot.intakeV4B.setWristDefaultPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositV4B.setWristPickPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositSlide.retractDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4)
+
+                ))
                 .strafeToLinearHeading(new Vector2d(85.5, 58.89), 0) //second sample
-                .stopAndAdd(new Action() { //intake slides go out
-                    @Override
-                    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-                        robot.intakeSlide.extendMainSlide();
-                        robot.intakeClaw.openClaw();
-                        robot.intakeClaw.setOrientationIncrease();
-                        robot.intakeV4B.setWristPickPosition();
-                        robot.intakeClaw.closeClaw();
-                        robot.intakeV4B.setWristDropPosition();
-                        robot.depositClaw.openDepositClaw();
-                        robot.intakeClaw.setOrientationIncrease();
-                        return true;
-                    }})
+                .stopAndAdd(new SequentialAction(
+                        (p) -> {
+                            robot.intakeSlide.extendMainSlide();
+                            robot.intakeClaw.openClaw();
+                            return false;
+                        },
+                        new SleepAction(2),
+                        (p) -> {
+                            robot.intakeClaw.setOrientationIncrease();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeV4B.setWristPickPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeClaw.closeClaw();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeV4B.setWristDropPosition();
+                            robot.depositClaw.openDepositClaw();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeClaw.setOrientationIncrease();
+                            return false;
+                        },
+                        new SleepAction(1)
+
+                ))
                 .strafeToLinearHeading(new Vector2d(72.0, 54.7), -45) //going back under basket
-                .stopAndAdd(new Action() {
-                    @Override
-                    public boolean run(@NonNull TelemetryPacket telemetryPacket) { //dep slides to drop pos
-                        robot.depositSlide.extendDepositMainSlide();
-                        robot.depositV4B.setWristDropPosition();
-                        robot.depositClaw.openDepositClaw();
-                        robot.intakeV4B.setWristDefaultPosition();
-                        robot.depositV4B.setWristPickPosition();
-                        robot.depositSlide.retractDepositMainSlide();
-                        return true;
-                    }})
+                .stopAndAdd(new SequentialAction(
+                        (p) -> {
+                            robot.depositSlide.extendDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4),
+                        (p) -> {
+                            robot.depositV4B.setWristDropPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositClaw.openDepositClaw();
+                            robot.intakeV4B.setWristDefaultPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositV4B.setWristPickPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositSlide.retractDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4)
+
+                ))
+
                 .strafeToLinearHeading(new Vector2d(95.86, 48.91), -80) //turn sideways and third sample
-                .stopAndAdd(new Action() { //intake slides go out
-                    @Override
-                    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-                        robot.intakeSlide.extendMainSlide();
-                        robot.intakeClaw.openClaw();
-                        robot.intakeClaw.setOrientationIncrease();
-                        robot.intakeV4B.setWristPickPosition();
-                        robot.intakeClaw.closeClaw();
-                        robot.intakeV4B.setWristDropPosition();
-                        robot.depositClaw.openDepositClaw();
-                        robot.intakeClaw.setOrientationIncrease();
-                        return true;
-                    }})
+                .stopAndAdd(new SequentialAction(
+                        (p) -> {
+                            robot.intakeSlide.extendMainSlide();
+                            robot.intakeClaw.openClaw();
+                            return false;
+                        },
+                        new SleepAction(2),
+                        (p) -> {
+                            robot.intakeClaw.setOrientationIncrease();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeV4B.setWristPickPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeClaw.closeClaw();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeV4B.setWristDropPosition();
+                            robot.depositClaw.openDepositClaw();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.intakeClaw.setOrientationIncrease();
+                            return false;
+                        },
+                        new SleepAction(1)
+
+                ))
                 .strafeToLinearHeading(new Vector2d(72.0, 54.7), -45) //going back under basket
-                .stopAndAdd(new Action() {
-                    @Override
-                    public boolean run(@NonNull TelemetryPacket telemetryPacket) { //dep slides to drop pos
-                        robot.depositSlide.extendDepositMainSlide();
-                        robot.depositV4B.setWristDropPosition();
-                        robot.depositClaw.openDepositClaw();
-                        robot.intakeV4B.setWristDefaultPosition();
-                        robot.depositV4B.setWristPickPosition();
-                        robot.depositSlide.retractDepositMainSlide();
-                        return true;
-                    }})
+                .stopAndAdd(new SequentialAction(
+                        (p) -> {
+                            robot.depositSlide.extendDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4),
+                        (p) -> {
+                            robot.depositV4B.setWristDropPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositClaw.openDepositClaw();
+                            robot.intakeV4B.setWristDefaultPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositV4B.setWristPickPosition();
+                            return false;
+                        },
+                        new SleepAction(1),
+                        (p) -> {
+                            robot.depositSlide.retractDepositMainSlide();
+                            return false;
+                        },
+                        new SleepAction(4)
+
+                ))
                 .strafeToLinearHeading(new Vector2d(115.52, 60.20), 0)//going to park part 1
                 .strafeToLinearHeading(new Vector2d(122.77, 26.01), 0)//going to park part 2
                 .build();
